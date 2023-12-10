6c8a673f04a3dbe388b243f175f98b5f7bd65a39
mark valdez <jhonmarkprudentevaldezcoder@gmail.com>
2023-12-10 19:02:56 +0800

    dada

--- models/reservedModel.js
+++ models/reservedModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const reservationSchema = mongoose.Schema(
   {
-    userid: {
+    userId: {
       type: String,
     },
     event: {
@@ -11,7 +11,6 @@ const reservationSchema = mongoose.Schema(
     theme: {
       type: String,
     },
-
     orderid: {
       type: String,
     },
